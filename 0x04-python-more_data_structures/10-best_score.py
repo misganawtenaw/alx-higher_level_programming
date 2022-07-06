615318aed5344f22481ff90f760f76c9dc4771c3
misganaw tenaw <misganawtenaw@gmail.com>
2022-07-06 10:25:12 -0700

    0x04-python-more_data_structures

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,15 +1,13 @@
 #!/usr/bin/python3
 
 def best_score(a_dictionary):
-    if a_dictionary == None or len(a_dictionary) == 0:
+    if not isinstance(a_dictionary, dict) or len(a_dictionary) == 0:
         return None
 
-    current_high_score = 0
-    high_score_holder = None
-
-    for key in a_dictionary:
-        if a_dictionary[key] > current_high_score:
-            current_high_score = a_dictionary[key]
-            high_score_holder =  key
-
-    return high_score_holder
+    val = list(a_dictionary.keys())[0]
+    biggest = a_dictionary[val]
+    for i, j in a_dictionary.items():
+        if j > biggest:
+            biggest = j
+            val = i
+    return (val)
