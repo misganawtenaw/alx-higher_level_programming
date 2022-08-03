1007a06b98929e78bc8d3e590a6535f854a4e86a
misganawtenaw <misganawtenaw@gmail.com>
2022-08-03 05:39:26 -0700

    0x0B-python-input_output

---
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+Module 5-save-to_json_file
+"""
+
+
+import json
+
+
+def save_to_json_file(my_obj, filename):
+    """ writes an Object to a text file, using a JSON representation """
+    with open(filename, mode='w', encoding='utf-8') as f:
+        json.dump(my_obj, f)
