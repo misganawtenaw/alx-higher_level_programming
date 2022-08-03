f21469f86e08e7fad1b197bc62848cdba68d4de2
misganawtenaw <misganawtenaw@gmail.com>
2022-08-03 04:25:09 -0700

    0x0A-python-inheritance

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""
+Module 2-is_same_class.py
+"""
+
+
+def is_same_class(obj, a_class):
+    """ returns exactly an instance of the specified class """
+    return type(obj) == a_class
