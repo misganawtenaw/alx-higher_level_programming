8b19578255f714e589e900f58a9e907d39316990
misganawtenaw <misganawtenaw@gmail.com>
2022-08-03 04:33:29 -0700

    0x0A-python-inheritance

---
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+Module 8-rectangle
+"""
+
+
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """inherits from BaseGeometry
+    Methods: __init__(self, width, height)
+    """
+
+    def __init__(self, width, height):
+        """validate and initialize width and height
+        Args:
+            width (int): private
+            height (int): private
+        """
+        super().integer_validator("width", width)
+        self.__width = width
+        super().integer_validator("height", height)
+        self.__height = 
