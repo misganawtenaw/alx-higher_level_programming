c40e9ca0b03e4350a3d480927a9f1374b5ab237c
misganawtenaw <misganawtenaw@gmail.com>
2022-09-06 07:30:30 -0700

    0x0D-SQL_introduction

--- 0x0D-SQL_introduction/102-top_city.sql
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -1,2 +1,3 @@
--- displays the top 3 of cities temperature during July and August ordered by temperature (descending)
-SELECT city, AVG(value) AS avg_temp FROM temperatures WHERE month = 6 AND month = 7 GROUP BY city ORDER BY avg_temp DESC LIMIT 3;
+-- Import in hbtn_0c_0 database this table dump
+-- Displays avg temp in July and August
+SELECT city, AVG(value) AS avg_temp FROM temperatures WHERE month=7 OR month=8 GROUP BY city ORDER BY avg_temp DESC LIMIT 3;
