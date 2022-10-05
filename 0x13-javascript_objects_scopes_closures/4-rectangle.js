8b874cf6d64b13bf51bac38d99d75d585ea9a6ab
misganawtenaw <misganawtenaw@gmail.com>
2022-10-05 01:42:15 -0700

    task 4

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,32 @@
+#!/usr/bin/node
+
+
+module.exports = class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    let char = '';
+    for (let i = 0; i < this.width; i++) {
+      char = char + 'X';
+    }
+    for (let i = 0; i < this.height; i++) {
+      console.log(char);
+    }
+  }
+
+  rotate () {
+    const tmp = this.width;
+    this.width = this.height;
+    this.height = tmp;
+  }
+
+  double () {
+    this.width *= 2;
+    this.height *= 2;
+  }
+};
