5f45940184d5c9a2fc5ef8ed996e740ad94e44e6
misganawtenaw <misganawtenaw@gmail.com>
2022-10-05 01:06:21 -0700

    task_15

---
+++ 0x12-javascript-warm_up/101-call_me_moby.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+/*
+Write a function that executes x times a function.
+    The function must be visible from outside
+*/
+
+exports.callMeMoby = function (x, theFunction) {
+  for (let i = 0; i < x; i++) {
+    theFunction();
+  }
+};
